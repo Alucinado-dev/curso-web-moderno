7f15fde91e72468c7df257905127e6183aa7087d
Lucino Campos <lucinogabriel1510@gmail.com>
2025-04-11 16:23:17 -0300

    mais anotações

--- exercicios-web/funcao/thisEBind1.js
+++ exercicios-web/funcao/thisEBind1.js
@@ -6,8 +6,12 @@ const pessoa = {
 }
 
 pessoa.falar()
-const falar = pessoa.falar
+const falar = pessoa.falar   /* quando a função é chamada aqui, o this aponta pro contexto atual, e não pra pessoa */
 falar() // conflito entre paradigmas: funcional e OO
 
-const falarDePessoa = pessoa.falar.bind(pessoa)
+const falarDePessoa = pessoa.falar.bind(pessoa) /* o método bind faz com que o this sempre aponte pro objeto selecionado */
 falarDePessoa()
+
+
+const qualOThis = () => console.log(this)
+qualOThis()
